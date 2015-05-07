fdf6c2b778892a35112e8de86ded82f71cd07a9e
thori0908 <mr.takumi.pc.0908@gmail.com>
2015-05-07 18:59:41 +0900

    ref #55 11 番の要素を jQuery を使って時計回り方向に 10 度回転させる

--- public/stage2/tests.js
+++ public/stage2/tests.js
@@ -195,7 +195,7 @@ describe('ステージ2（意図した通りに DOM 要素の属性・テキス�
 
       // ここにコードを記述してください。
       // 変更した DOM 要素は $element 変数に代入してください。
-      var $element = 'change me!';
+      var $element = $("p").css('transform', "rotate(" + 10 + "deg)");
 
 
       expect($element).to.be.instanceof(jQuery);
